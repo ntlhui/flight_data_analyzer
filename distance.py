16e38c03bffb30912d1626bb17aa1e904004d01b
Nathan Hui <ntlhui@gmail.com>
2019-05-27 14:01:02 -0700

    Updated distance

--- distance.py
+++ distance.py
@@ -8,6 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 from multiprocessing import Pool, Queue
 from itertools import product
+import utm
+
+import argparse
 
 def process_ac(logfile, output_file):
 	'''Processes the provided ArduCopter logfile and outputs the distance flown
@@ -33,7 +36,8 @@ def process_ac(logfile, output_file):
 	positions = []
 	for i in range(len(positions_unfiltered)):
 		if positions_unfiltered[i,0] != 0:
-			positions.append(positions_unfiltered[i,:])
+			utm_coord = utm.from_latlon(positions_unfiltered[i,0], positions_unfiltered[i,1])
+			positions.append([utm_coord[0], utm_coord[1], positions_unfiltered[i, 2]])
 	if len(positions) == 0:
 		return
 	positions = np.array(positions)
@@ -71,7 +75,9 @@ def process_dji(logfile, output_file):
 	positions = []
 	for i in range(len(positions_unfiltered)):
 		if positions_unfiltered[i,0] != 0:
-			positions.append(positions_unfiltered[i,(1,2,3)])
+			utm_coord = utm.from_latlon(positions_unfiltered[i,2], positions_unfiltered[i,1])
+			positions.append([utm_coord[0], utm_coord[1], positions_unfiltered[i, 2]])
+
 	if len(positions) == 0:
 		return
 	positions = np.array(positions)
@@ -98,15 +104,21 @@ def plot_data(output_file):
 	plt.xlabel('Distance (m)')
 	plt.ylabel('Number of Flights')
 	plt.title('Distribution of Flight Distances')
-	return fig
+	return fig, q_distances
 
 
 if __name__ == '__main__':
 
-	data_dir = '/home/ntlhui/googledrive'
+	parser = argparse.ArgumentParser(description='Calculates the distribution of distance flown in the specified logs')
+	parser.add_argument('--input_dir', '-i', metavar='input_dir', help='Directory containing logs', default='/home/ntlhui/googledrive')
+	parser.add_argument('--output_file', '-o', metavar='output_file', help='Path to histogram filename', default='./distance.png')
+
+	args = parser.parse_args()
+
+	data_dir = args.input_dir
 	arducopterlogs = glob.glob(os.path.join(data_dir, "**", "*.BIN"), recursive=True) + glob.glob(os.path.join(data_dir, "**", "*.bin"), recursive=True)
 	djilogs = glob.glob(os.path.join(data_dir, "**", "*.csv"), recursive=True)
-	output_dir = '/home/ntlhui/workspace/ECE143/'
+	output_dir = '/tmp/'
 
 	output_file = os.path.join(output_dir, 'distances.csv')
 	with open(output_file, 'w') as file:
@@ -118,7 +130,18 @@ if __name__ == '__main__':
 	p.starmap(process_ac, process_ac_input)
 	p.starmap(process_dji, process_dji_input)
 
-	fig = plot_data(output_file)
-	plt.figure(fig)
-	plt.savefig('distance.png')
+	fig, distances = plot_data(output_file)
+	os.remove(output_file)
+	plt.figure(fig.number)
+	plt.savefig(args.output_file)
 	plt.close(fig)
+
+	mean = np.mean(distances)
+	median = np.median(distances)
+	max_d = np.max(distances)
+	min_d = np.min(distances[np.nonzero(distances)])
+
+	print('Mean distance: %.0f m' % (mean))
+	print('Median distance: %.0f m' % (median))
+	print('Max distance: %.0f m' % (max_d))
+	print('Min distance: %.0f m' % (min_d))
