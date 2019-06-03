ce5c36acd762a55f79b6d83798200ff52ac9a34e
kazimergun <kazimergun@gmail.com>
2019-06-03 15:53:31 -0700

    Added Kazim's scripts

---
+++ data_analysis_kazim.py
@@ -0,0 +1,116 @@
+from arducopter_extract import ArduLog
+import pandas as pd
+import glob
+import numpy as np
+import os
+
+
+def get_files():
+    #files = glob.glob("/root/gdrive" + '/**/*.bin', recursive=True) + glob.glob("/root/gdrive" + '/**/*.BIN', recursive=True)
+    files = glob.glob("/root/gdrive" + '/**/*.BIN', recursive=True)
+
+    return files
+
+
+
+def get_altitudes(files):
+
+    field = 'GPS'
+    field = 'AHR2'
+
+    df_list = []
+    for file in files:
+        try:
+            if field == 'GPS':
+                poses = ArduLog(file).extract_6dof1()
+                df = pd.DataFrame(poses, columns = ['TimeMS','Lat','Lng','RelAlt','T'])
+                df = df[['TimeMS','RelAlt']].groupby(df.index // 5).agg({'TimeMS':'mean','RelAlt':'mean'})
+                df['TimeMS'] = (df['TimeMS'] - df['TimeMS'].min()) / 1000
+                df.name = os.path.basename(file)    
+                df_list.append(df)
+            if field == 'AHR2':
+                poses = ArduLog(file).extract_6dof3()
+                df = pd.DataFrame(poses, columns = ['TimeMS','Roll','Pitch','Yaw','Alt','Lat','Lng'])
+                df = df[['TimeMS','Alt']].groupby(df.index // 5).agg({'TimeMS':'mean','Alt':'mean'})
+                df['TimeMS'] = (df['TimeMS'] - df['TimeMS'].min()) / 1000
+                df.name = os.path.basename(file)
+                df_list.append(df)
+        except KeyboardInterrupt:
+            raise    
+        except:
+            pass
+    
+    return df_list
+
+
+def get_dates(files):
+    i = 1
+    df = pd.DataFrame(columns = ['Flight Date','Number of Flights','Takeoff Times','Flight Durations'])
+    for file in files:
+        try:
+            print(i)
+            takeoffdate, takeoff_times, landing_times = ArduLog(file).extract_takeoffs()
+            durations = [landing_time - takeoff_time for landing_time, takeoff_time in zip(landing_times,takeoff_times)]
+            durations = [duration.total_seconds() for duration in durations]
+            takeoff_times = [takeoff_time.strftime('%H:%M:%S') for takeoff_time in takeoff_times]
+            #landing_times = [landing_time.strftime('%H:%M:%S') for landing_time in landing_times]
+            df.loc[os.path.basename(file)] = [takeoffdate,len(takeoff_times),takeoff_times,durations]
+            i+=1
+        except KeyboardInterrupt:
+            raise    
+        except:
+            pass
+    return df
+
+def get_currents(files):
+
+    field = 'CURR'
+
+    df_list = []
+    for file in files:
+        try:
+            if field == 'CURR':
+                poses = ArduLog(file).extract_current()
+                df = pd.DataFrame(poses, columns = ['TimeMS','Curr'])
+                df = df[['TimeMS','Curr']].groupby(df.index // 5).agg({'TimeMS':'mean','Curr':'mean'})
+                df['TimeMS'] = (df['TimeMS'] - df['TimeMS'].min()) / 1000
+                df.name = os.path.basename(file)    
+                df_list.append(df)
+        except KeyboardInterrupt:
+            raise    
+        except:
+            pass
+    
+    return df_list
+
+def altitude_brackets(altitudes,currents):
+    cond = 'Curr < 500'
+    brackets = ['ground','mission']
+    df_list = []
+    for df_altitude, df_current in zip(altitudes,currents):
+        df_current['cond'] = df_current.eval(cond)
+        #grp.append(df_current.groupby('cond'))
+        df_current['changed'] = df_current['cond'].ne(df_current['cond'].shift().bfill()).astype(int)
+        grp = df_current.groupby('changed')
+        try:
+            changes = grp.get_group(1)
+            prev = 0
+            for index, row in changes.iterrows():
+                df_altitude.loc[(df_altitude['TimeMS']>=prev)&(df_altitude['TimeMS']<row[0]),'bracket'] = brackets[index%2]
+                prev = row[0]
+            df_altitude.loc[df_altitude['TimeMS']>=prev,'bracket'] = 'ground'
+            df_list.append(df_altitude)
+        except:
+            df_altitude['bracket'] = 'ground'
+            df_list.append(df_altitude)
+    return df_list
+
+
+
+if __name__ == '__main__':
+    files = get_files()
+  #  df_altitudes = get_altitudes(files[:3])
+  #  df_takeoff = get_dates(files)
+  #  df_takeoff.to_pickle("./dates.pkl")
+  #  df_currents = get_currents(files[:3])
+  #  df_brackets = altitude_brackets(df_altitudes,df_currents)
---
+++ plot_flighttimes.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import calmap
+import matplotlib.pyplot as plt
+import hvplot
+import hvplot.pandas
+import holoviews as hv
+
+
+
+df = pd.read_pickle('dates.pkl')
+# df = df[['Flight Date','Flight Durations']]
+# df = df[~df['Flight Date'].isnull()]
+# df = df.groupby('Flight Date').agg(sum)
+# df['Flight Durations'] = df['Flight Durations'].apply(lambda x: sorted(x))
+
+# for i in range(0,len(df['Flight Durations'])):
+#     old_list = df['Flight Durations'][i]
+#     new_list = [x for x in old_list if x>20]
+#     df['Flight Durations'][i] = new_list
+
+# flights = df['Flight Durations'].apply(pd.Series)
+# flights = flights.rename(columns = lambda x : 'Flight ' + str(x))
+
+# #%opts Bars [tools=['hover'],legend_position='left',color_index='Variable',width=900,height=400](alpha=0.5,color=hv.Palette('Category20'))
+# flights.hvplot.bar(stacked=True,rot=45).redim(value=hv.Dimension('value',label='Duration')).relabel('Duration')
+
+
+#flights.hvplot.bar(stacked=True)
